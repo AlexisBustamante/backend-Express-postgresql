0f93a824942c52eeec23ddf9d98af6861530514e
Alexis Bustamante <abustamante@colegium.com>
2022-09-19 18:03:04 -0300

    migraciones

--- db/migrations/20220919191033-unica.js
+++ db/migrations/20220919191033-unica.js
@@ -5,23 +5,47 @@ const { ProductSchema, PRODUCT_TABLE } = require('./../models/product.model');
 const { CategorySchema, CATEGORY_TABLE } = require('./../models/category.model');
 const { OrderSchema, ORDER_TABLE } = require('./../models/order.model');
 const { OrderProductSchema, ORDER_PRODUCT_TABLE } = require('./../models/order-product.model');
-
 module.exports = {
-  async up(queryInterface) {
+  up: async (queryInterface) => {
     await queryInterface.createTable(USER_TABLE, UserSchema);
     await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
     await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
     await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+    });
     await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
+
   },
 
-  async down(queryInterface) {
-    await queryInterface.dropTable(USER_TABLE);
-    await queryInterface.dropTable(CUSTOMER_TABLE);
-    await queryInterface.dropTable(PRODUCT_TABLE);
-    await queryInterface.dropTable(CATEGORY_TABLE);
-    await queryInterface.dropTable(ORDER_TABLE);
-    await queryInterface.dropTable(ORDER_PRODUCT_TABLE);
+  down: async (queryInterface) => {
+    await queryInterface.dropTable(USER_TABLE)
+    await queryInterface.dropTable(CUSTOMER_TABLE)
+    await queryInterface.dropTable(CATEGORY_TABLE)
+    await queryInterface.dropTable(PRODUCT_TABLE)
+    await queryInterface.dropTable(ORDER_TABLE)
+    await queryInterface.dropTable(ORDER_PRODUCT_TABLE)
   }
 };
