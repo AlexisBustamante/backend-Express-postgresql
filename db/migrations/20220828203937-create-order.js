0d49649153d9a3b4c8e16e8c230a7076b6054802
Alexis Bustamante <abustamante@colegium.com>
2022-09-19 19:19:06 -0300

    cambios migracion nuevos

--- db/migrations/20220828203937-create-order.js
+++ db/migrations/20220828203937-create-order.js
@@ -1,9 +1,37 @@
 'use strict';
 const { OrderSchema, ORDER_TABLE } = require('./../models/order.model');
+const { CUSTOMER_TABLE } = require('./../models/customer.model');
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     //aca creara las tablas.
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+
+    });
   },
 
   async down(queryInterface, Sequelize) {
