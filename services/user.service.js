bb4e4b684e8fdbe31675105607fb437a30556108
Alexis Bustamante <abustamante@colegium.com>
2024-12-26 14:02:42 -0300

    fix: error en la consulta

--- services/user.service.js
+++ services/user.service.js
@@ -19,7 +19,7 @@ class UserService {
   async find(id) {
     const queryOptions = {
       include: ['customer'],
-      order: [[orderBy, 'ASC']],//Ordena por defecto por ID en orden ascendente
+      order: [['createdAt', 'DESC']],//Ordena por defecto por ID en orden ascendente
     };
   
     // Si se pasa un ID, excluye el registro con ese ID
