af511bed95c06c46a64808e2cbb6605400115bd7
Alexis Bustamante <abustamante@colegium.com>
2025-01-13 08:09:45 -0300

    fix: incluyo en whilist la url de la intranet 1

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(helmet());
 app.use(express.json());
 app.disable('x-powered-by');
 //para validar desde que aplicaicon pueden preguntar a la api, solo 
-const whitelist = ['http://localhost:8080', 'https://myapp.co','http://localhost:6060'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co','http://localhost:6060','https://intranet-1-dfnj.onrender.com'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
