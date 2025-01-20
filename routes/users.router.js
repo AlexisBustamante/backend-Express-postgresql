f6bf5444c1a8fa0b48535f7dc906ae492cb495d6
Alexis Bustamante <abustamante@colegium.com>
2025-01-20 12:39:46 -0300

    fix:error envio mail

--- routes/users.router.js
+++ routes/users.router.js
@@ -81,6 +81,7 @@ router.post('/',
 
       //ACA ENVIAMOS EL CORREO A AL PERSONA QUE INGRESO LA SOLICITUD
       const r = await serviceAuth.sendMail(mail);
+      console.log("RESPUESTA",r);
       res.status(201).json(newUser);
     } catch (error) {
       next(error);
